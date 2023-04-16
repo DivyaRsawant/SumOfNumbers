cb7fc9c9c509df22915dde525b169ecbfb4ec39d
Divya Sawant <86359301+DivyaRsawant@users.noreply.github.com>
2023-04-16 14:30:19 +0530

    Add files via upload

---
+++ SumOfNumbers.py
@@ -0,0 +1,18 @@
+# sum of a given number 3=6 OR 4=4+3+2+1=10
+# num = int(input("Enter a number :"))
+# sum = 0
+#
+# if (num < 0):
+#     print("Enter a positive number")
+# else:
+#     while (num > 0):
+#         sum += num
+#         num -= 1
+#     print("The sum of all number is :", sum)
+
+# sum of two numbers
+num1 = int(input("Enter num 1 :"))
+num2 = int(input("Enter num 2 :"))
+
+sum = num1 + num2
+print("The sum of", num1, "and", num2, "is", sum)
